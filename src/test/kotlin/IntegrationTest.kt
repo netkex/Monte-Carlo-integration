271f5f008552aa82f456b1c32c299ac8aa11019e
Daniil Pavlenko <dp.3481088@gmail.com>
2022-02-14 21:50:00 +0300

    add tests update README.md add workflow/check.yml

--- src/test/kotlin/IntegrationTest.kt
+++ src/test/kotlin/IntegrationTest.kt
@@ -8,7 +8,7 @@ internal class IntegrateTest {
 
     private fun assertEqualsEps(expected: Double, actual: Double, epsilon: Double) {
         assertTrue(
-            abs(expected - actual) < min(epsilon, expected * epsilon),
+            abs(expected - actual) < max(epsilon, expected * epsilon),
             "expected <$expected>, actual <$actual>, eps <$epsilon>, \n" +
                     "relative eps <${expected * epsilon}>, diff <${abs(expected - actual)}>")
     }
@@ -36,6 +36,35 @@ internal class IntegrateTest {
 
         val res = 2 * PI / 3
         assertEqualsEps(res, integrate2D(::fTest, ::pStandard, N=iterations), eps)
+    }
+
+    @Test
+    fun `integral with non-standard deviation`() {
+        fun fTest(pnt: Point2D): Double {
+            return if (abs(pnt.x) > 1 || abs(pnt.y) > 1) 0.0 else pnt.x.pow(3) + 5 * pnt.y.pow(2)* pnt.x + 3
+        }
+
+        fun pTest(pnt: Point2D): Double {
+            return if (abs(pnt.x) > 2 || abs(pnt.y) > 2) 0.0 else 1.0 / 16
+        }
+
+        val res = 12.0
+        assertEqualsEps(res, integrate2D(::fTest, ::pTest, N=iterations), eps)
+    }
+
+    @Test
+    fun `integral with non-standard deviation not centered at 0`() {
+        fun fTest(pnt: Point2D): Double {
+            return if (abs(pnt.x - 5) > 1 || abs(pnt.y - 3) > 1) 0.0
+            else (pnt.x - 5).pow(3) + 5 * (pnt.y - 3).pow(2)* (pnt.x - 5) + 3
+        }
+
+        fun pTest(pnt: Point2D): Double {
+            return if (abs(pnt.x - 5) > 2 || abs(pnt.y - 3) > 2) 0.0 else 1.0 / 16
+        }
+
+        val res = 12.0
+        assertEqualsEps(res, integrate2D(::fTest, ::pTest, firstPoint=Point2D(5.0, 3.0), N=iterations), eps)
     }
 
 }
