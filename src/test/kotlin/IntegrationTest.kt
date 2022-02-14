92951ae64b2f5af984bcaed4635820e365f43cf5
Daniil Pavlenko <dp.3481088@gmail.com>
2022-02-14 21:58:44 +0300

    update IntegrationTest.kt update README.md

--- src/test/kotlin/IntegrationTest.kt
+++ src/test/kotlin/IntegrationTest.kt
@@ -41,6 +41,35 @@ internal class IntegrateTest2D: IntegrateTest() {
         val res = 2 * PI / 3
         assertEqualsEps(res, integrate(::fTest, ::pStandard, N=iterations), eps)
     }
+
+    @Test
+    fun `integral with non-standard deviation`() {
+        fun fTest(pnt: Point2D): Double {
+            return if (abs(pnt.x) > 1 || abs(pnt.y) > 1) 0.0 else pnt.x.pow(3) + 5 * pnt.y.pow(2)* pnt.x + 3
+        }
+
+        fun pTest(pnt: Point2D): Double {
+            return if (abs(pnt.x) > 2 || abs(pnt.y) > 2) 0.0 else 1.0 / 16
+        }
+
+        val res = 12.0
+        assertEqualsEps(res, integrate(::fTest, ::pTest, N=iterations), eps)
+    }
+
+    @Test
+    fun `integral with non-standard deviation not centered at 0`() {
+        fun fTest(pnt: Point2D): Double {
+            return if (abs(pnt.x - 5) > 1 || abs(pnt.y - 3) > 1) 0.0
+            else (pnt.x - 5).pow(3) + 5 * (pnt.y - 3).pow(2)* (pnt.x - 5) + 3
+        }
+
+        fun pTest(pnt: Point2D): Double {
+            return if (abs(pnt.x - 5) > 2 || abs(pnt.y - 3) > 2) 0.0 else 1.0 / 16
+        }
+
+        val res = 12.0
+        assertEqualsEps(res, integrate(::fTest, ::pTest, firstPoint=Point2D(5.0, 3.0), N=iterations), eps)
+    }
 }
 
 internal class IntegrateTest1D: IntegrateTest() {
@@ -101,6 +130,4 @@ internal class IntegrateTest3D: IntegrateTest() {
         val res = PI.pow(3.0 / 2) / 8
         assertEqualsEps(res, integrate(::fTest, ::pStandard, N=iterations), eps)
     }
-
-
 }
